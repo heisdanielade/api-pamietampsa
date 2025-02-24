a3f745e32efdf39805baaa02565926873f1ee057
heisdanielade <heisdanielade@gmail.com>
2025-02-24 23:53:56 +0100

    initial commit

---
+++ src/main/java/com/github/heisdanielade/pamietampsa/PamietampsaApplication.java
@@ -0,0 +1,13 @@
+package com.github.heisdanielade.pamietampsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamietampsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamietampsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/heisdanielade/pamietampsa/PamietampsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.heisdanielade.pamietampsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamietampsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
