379e696905dbd03f9d798525da790dfe2639124b
heisdanielade <heisdanielade@gmail.com>
2025-02-25 01:05:08 +0100

    Fix: created script to run build and run jar file

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -jar target/pamietampsa-0.0.1-SNAPSHOT.jar
--- src/main/java/com/github/heisdanielade/pamietampsa/PamietampsaApplication.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/PamietampsaApplication.java
@@ -3,10 +3,12 @@ package com.github.heisdanielade.pamietampsa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PamietampsaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PamietampsaApplication.class, args);
 	}
 
