ba0483d5c5653383b4378c76d6c544d897ef5824
heisdanielade <heisdanielade@gmail.com>
2025-02-25 01:26:43 +0100

    Update: added User Entity

---
+++ src/main/java/com/github/heisdanielade/pamietampsa/entity/User.java
@@ -0,0 +1,51 @@
+package com.github.heisdanielade.pamietampsa.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 10
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String email;
+    private String username;
+    private Character initial;
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @Column
+    private Instant lastLoginAt; // to set manually
+
+
+
+
+}
