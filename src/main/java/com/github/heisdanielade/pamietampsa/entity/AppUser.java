3e73b131c6461ac93b9aa85940df78114feb7962
heisdanielade <heisdanielade@gmail.com>
2025-03-02 22:24:39 +0100

    Update: implemented UserDetails interface for AppUser and Added user Role enum

--- src/main/java/com/github/heisdanielade/pamietampsa/entity/AppUser.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/entity/AppUser.java
@@ -3,20 +3,26 @@ package com.github.heisdanielade.pamietampsa.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Entity
 @Table
-public class AppUser {
+public class AppUser implements UserDetails {
 
     @Id
     @SequenceGenerator(
@@ -37,6 +43,8 @@ public class AppUser {
 
     @Column(nullable = false)
     private String password; // to hash with BCrypt
+
+//    private Role role;
 
     @CreationTimestamp
     @Column(updatable = false, nullable = false)
@@ -59,4 +67,28 @@ public class AppUser {
         return this.username.charAt(0);
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
---
+++ src/main/java/com/github/heisdanielade/pamietampsa/enums/Role.java
@@ -0,0 +1,6 @@
+package com.github.heisdanielade.pamietampsa.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
