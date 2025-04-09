87d7bce205eece6c118e400fc5d5eb30a371ae76
heisdanielade <heisdanielade@gmail.com>
2025-04-10 01:17:45 +0200

    update: added generic api response template class for all api responses

---
+++ src/main/java/com/github/heisdanielade/pamietampsa/response/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.github.heisdanielade.pamietampsa.response;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    private T data;
+
+    public ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+        System.out.println("============ Serializing ApiResponse: " + message);
+    }
+
+    public ApiResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+        System.out.println("============ Serializing ApiResponse: " + message);
+    }
+}
