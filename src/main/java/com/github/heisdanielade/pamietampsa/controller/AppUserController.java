ac5bf0dda228f57e5483cc3b8ec8f9087817d0cb
heisdanielade <heisdanielade@gmail.com>
2025-04-17 01:18:32 +0200

    update: added base user info

--- src/main/java/com/github/heisdanielade/pamietampsa/controller/AppUserController.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/controller/AppUserController.java
@@ -1,14 +1,18 @@
 package com.github.heisdanielade.pamietampsa.controller;
 
 import com.github.heisdanielade.pamietampsa.entity.AppUser;
+import com.github.heisdanielade.pamietampsa.response.ApiResponse;
 import com.github.heisdanielade.pamietampsa.service.AppUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,20 +21,30 @@ public class AppUserController {
 
     private final AppUserService appUserService;
 
-    @GetMapping(path = "/hello")
-    public String hello(){
-        return "Hello World!";
-    }
-
-    @GetMapping("/me")
-    public ResponseEntity<AppUser> authenticatedUser(){
+    @GetMapping(path = "/user")
+    public ResponseEntity<ApiResponse<Map<String, Object>>> baseUserInfo(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         AppUser currentUser = (AppUser) authentication.getPrincipal();
-        return ResponseEntity.ok(currentUser);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("email", currentUser.getEmail());
+        data.put("name", currentUser.getName());
+        data.put("initial", currentUser.getInitial());
+        data.put("enabled", currentUser.isEnabled());
+        data.put("role", currentUser.getRole());
+
+        ApiResponse<Map<String, Object>> response = new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "Currently logged in user details.",
+                data
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
-    @GetMapping("/")
+
+    @GetMapping("/users/all")
     public ResponseEntity<List<AppUser>> allUsers(){
         List<AppUser> users = appUserService.allUsers();
         return ResponseEntity.ok(users);
