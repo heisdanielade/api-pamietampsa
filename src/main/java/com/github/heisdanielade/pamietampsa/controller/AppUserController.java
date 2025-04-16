14f6b2b8b014802aff0aaf09dcfe6532a6f078bc
heisdanielade <heisdanielade@gmail.com>
2025-04-16 18:56:20 +0200

    minor changes

--- src/main/java/com/github/heisdanielade/pamietampsa/controller/AppUserController.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/controller/AppUserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/v1", produces = "application/json")
 public class AppUserController {
--- src/main/java/com/github/heisdanielade/pamietampsa/service/EmailService.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/service/EmailService.java
@@ -13,13 +13,14 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendVerificationEmail(String to, String subject, String text) throws MessagingException{
+
+    public void sendVerificationEmail(String to, String subject, String body) throws MessagingException{
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         helper.setTo(to);
         helper.setSubject(subject);
-        helper.setText(text, true);
+        helper.setText(body, true);
 
         emailSender.send(message);
     }
