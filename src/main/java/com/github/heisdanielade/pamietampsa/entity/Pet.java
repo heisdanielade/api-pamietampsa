11f5d00c27d9294f8d80e6f155cef748840fb54d
heisdanielade <heisdanielade@gmail.com>
2025-05-05 23:29:29 +0200

    fix: infinite recursion output in JSON serialization

--- src/main/java/com/github/heisdanielade/pamietampsa/entity/Pet.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/entity/Pet.java
@@ -1,5 +1,6 @@
 package com.github.heisdanielade.pamietampsa.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -35,6 +36,7 @@ public class Pet {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "app_user_id", nullable = false)
+    @JsonIgnore
     private AppUser owner;
 
 
