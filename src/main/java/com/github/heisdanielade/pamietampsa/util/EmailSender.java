b784fce59cc781f743bf4151e86ad9abbbccd94c
heisdanielade <heisdanielade@gmail.com>
2025-05-11 21:36:01 +0200

    update: added pet registration confirmation method to emailSender

--- src/main/java/com/github/heisdanielade/pamietampsa/util/EmailSender.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/util/EmailSender.java
@@ -1,26 +1,58 @@
 package com.github.heisdanielade.pamietampsa.util;
 
-import com.github.heisdanielade.pamietampsa.entity.AppUser;
 import com.github.heisdanielade.pamietampsa.service.EmailService;
 import jakarta.mail.MessagingException;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
+@Component
+public class EmailSender {
 
-public class EmailSenders {
+    private final EmailService emailService;
 
-    private EmailService emailService;
+    public EmailSender(EmailService emailService){
+        this.emailService = emailService;
+    }
 
-    public void sendVerificationEmail(AppUser user, String subject){
+    private void sendTemplatedEmail(String to, String subject, String templatePath, Map<String, String> placeholders) {
         try {
-            String htmlMessage = new String(Files.readAllBytes(Paths.get("src/main/resources/templates/email/account/registration.html")));
-            emailService.sendEmail(user.getEmail(), subject, htmlMessage);
-        } catch (IOException e){
+            String htmlMessage = new String(Files.readAllBytes(Paths.get(templatePath)));
+            if (placeholders != null) {
+                for (Map.Entry<String, String> entry : placeholders.entrySet()) {
+                    htmlMessage = htmlMessage.replace(entry.getKey(), entry.getValue());
+                }
+            }
+            emailService.sendEmail(to, subject, htmlMessage);
+        } catch (IOException e) {
             System.out.println("(e) Error loading email template: " + e.getMessage());
         } catch (MessagingException e) {
             System.out.println("(e) Error sending email: " + e.getMessage());
         }
     }
+
+
+    // User-related emails
+    public void sendVerificationEmail(String userEmail, String verificationCode){
+        String subject = verificationCode + " is your verification code";
+        Map<String, String> placeholders = Map.of("{{verification_code}}", verificationCode);
+        sendTemplatedEmail(userEmail, subject, "src/main/resources/templates/email/auth/email-verification.html", placeholders);
+    }
+    public void sendUserRegistrationConfirmationEmail(String userEmail){
+        String subject = "Welcome to PamietamPsa";
+        sendTemplatedEmail(userEmail, subject, "src/main/resources/templates/email/account/user-registration.html", null);
+    }
+
+    // Pet-related emails
+    public void sendPetRegistrationConfirmationEmail(String userEmail, String petName, String petSpecies){
+        String subject = "New Pet registration (" + petName + ")";
+        Map<String, String> placeholders = Map.of("{{petName}}", petName, "{{petSpecies}}", petSpecies);
+        sendTemplatedEmail(userEmail, subject, "src/main/resources/templates/email/pet/pet-registration.html", placeholders);
+    }
+
+
+
 }
