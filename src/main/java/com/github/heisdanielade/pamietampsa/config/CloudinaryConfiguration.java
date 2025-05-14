49191b0b94bcd5d8d485441847ef01fdeb0e328a
heisdanielade <heisdanielade@gmail.com>
2025-05-14 23:50:50 +0200

    update: added cloudinary dependency & configuration

--- src/main/java/com/github/heisdanielade/pamietampsa/config/CloudinaryConfiguration.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/config/CloudinaryConfiguration.java
@@ -1,4 +1,33 @@
 package com.github.heisdanielade.pamietampsa.config;
 
-public class CloudinaryConfig {
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfiguration {
+
+    private final String CLOUD_NAME;
+    private final String API_KEY;
+    private final String API_SECRET;
+
+    public CloudinaryConfiguration(
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret){
+        this.CLOUD_NAME = cloudName;
+        this.API_KEY = apiKey;
+        this.API_SECRET = apiSecret;
+    }
+
+    @Bean
+    public Cloudinary cloudinary(){
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", CLOUD_NAME,
+                "api_key", API_KEY,
+                "api", API_SECRET
+        ));
+    }
 }
