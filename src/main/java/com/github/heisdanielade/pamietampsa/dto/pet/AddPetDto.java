79b9de6ad41c338c10651e411f3793081141a4eb
heisdanielade <heisdanielade@gmail.com>
2025-05-15 09:40:24 +0200

    update: changed AddPetDto profileImage attrib type to MultiPartFile

--- src/main/java/com/github/heisdanielade/pamietampsa/dto/pet/AddPetDto.java
+++ src/main/java/com/github/heisdanielade/pamietampsa/dto/pet/AddPetDto.java
@@ -3,6 +3,7 @@ package com.github.heisdanielade.pamietampsa.dto.pet;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 public class AddPetDto {
     @NotBlank(message = "Name is required")
     private String name;
+    private MultipartFile profileImage;
     @NotBlank(message = "Species is required")
     private String species;
     private String breed;
